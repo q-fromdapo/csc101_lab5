c0f2c6788c190d1124d6ccc9f4eddcedb54d1930
Quinton <117963690+q-fromdapo@users.noreply.github.com>
2024-04-11 20:55:51 +0900

    Update qutinton_mclain_lab5_code.py
    added constant base prices and additional costs

--- qutinton_mclain_lab5_code.py
+++ qutinton_mclain_lab5_code.py
@@ -4,3 +4,11 @@ Program Title: T-Shirt Shop
 File Description:
 The program will help Matthew keep track of the T-shirt sales at his stoes.
 '''
+
+#CONSTANT BASE PRICES AND ADDITIONAL COSTS
+basic_price = 12.99
+graphic_price = 16.99
+band_price = 24.99
+medium_size_fee = 2.00
+large_size_fee = 4.00
+
